89ab7fb81c2ca0ebf7c85cd84d0370df7d2a85f3
Emper39 <07andersemil@gmail.com>
2024-04-25 11:10:07 +0200

    New Version
    Working Collisions

---
+++ Floppy dude/Assets/Scripts/LimbCollisions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbCollisions : MonoBehaviour
+
+{
+    public GameObject dude;
+    private void OnTriggerEnter2D(Collider2D trigger)  // Checks collision with both the ground and the pipes.
+    {
+        if (trigger.gameObject.tag != "Safe")
+        {
+            Lose();
+        }
+    }
+    public void Lose()
+    {
+        Destroy(dude);
+        Scene_Management.Load_Scene(2);
+    }
+}
--- Floppy dude/Assets/Scripts/Running.cs
+++ Floppy dude/Assets/Scripts/Running.cs
@@ -8,41 +8,19 @@ using UnityEngine.UI;
 public class Running : MonoBehaviour
 {
     public int playervel;
-    public Rigidbody2D Torso;
-    // Start is called before the first frame update
+    public List<Rigidbody2D> player = new List<Rigidbody2D>(); 
+    
     void Start()
     {
 
-        Torso = this.GetComponent<Rigidbody2D>();
-
-
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump") == true)
         {
-            Torso.velocity = Vector2.up * playervel;
+            player[0].velocity = Vector2.up * playervel;
         }
     }
-
-
-    private void OnTriggerEnter2D(Collider2D trigger)  // Checks collision with both the ground and the pipes.
-    {
-        if (trigger.gameObject.tag != "Safe")
-        {
-            Lose();
-        }
-    }
-
-    public void Lose()
-    {
-        Destroy(gameObject);
-        Scene_Management.Load_Scene(2);
-    }
-
-
-
-
 }
